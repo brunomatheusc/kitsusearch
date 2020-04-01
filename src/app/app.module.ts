06f869791b3696b41cb0c6351ae35ac24ba08894
Bruno Matheus <bmatheusc@gmail.com>
2020-04-01 16:43:08 -0300

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ListComponent,
+    DetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
